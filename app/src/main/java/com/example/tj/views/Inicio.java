5279e95f11f3ccdcdf73c544d37bb64aa8d0aa2a
tj <tj.batera1@gmail.com>
2017-10-03 08:48:30 -0300

    Exercício de Views

--- app/src/main/java/com/example/tj/views/Inicio.java
+++ app/src/main/java/com/example/tj/views/Inicio.java
@@ -1,7 +1,10 @@
 package com.example.tj.views;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Inicio extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        Button button = (Button) findViewById(R.id.Tela2);
+    }
+
+    public void tela2(View v){
+        Intent it = new Intent(this, Tela2.class);
+        startActivity(it);
     }
 }
